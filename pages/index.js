675b8b4fd98cda08b311b03ce0975494df3f0b19
Mohamad Salimi <mohamadsalimi151@gmail.com>
2024-02-09 18:25:19 +0330

    first commit

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,11 @@ const Home = () => {
   return (
     <div>
       <h1>Todo App</h1>
+      <p>
+        Todo-App is a daily task management application that allows users to
+        create, organize, and track their to-do lists. The application is
+        developed using web technologies and can be run on various web browsers.
+      </p>
     </div>
   );
 };
